5bdd14a2577a8b385ceed577d64108c04ede803b
Khushi <126047007+khushis15@users.noreply.github.com>
2023-04-07 09:33:43 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,16 @@
+ludogame = () => { 
+    const play1 = Math.floor(Math.random() * 6) + 1; 
+    const play1dice = `dice${play1}.jpg`;
+    document.getElementById('check1').setAttribute('src', play1dice);
+
+    const play2 = Math.floor(Math.random() * 6) + 1; 
+    const play2dice = `dice${play2}.jpg`;
+    document.getElementById('check2').setAttribute('src', play2dice);
+
+    if(play1 > play2){ 
+        document.querySelector('h1').innerHTML="Player 1 won !!";
+    } else if(play1 < play2) {
+        document.querySelector('h1').innerHTML="Player 2 won !!";
+    } else{ 
+        document.querySelector('h1').innerHTML="DRAW !!"; }
+}
